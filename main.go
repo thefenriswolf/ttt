f8c2d567a06204279d829019e98d2cc1b6baaef9
thefenriswolf <stefan.rohrbacher97@gmail.com>
2023-12-23 00:09:41 +0100

    refactoring

--- main.go
+++ main.go
@@ -4,40 +4,74 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 )
 
+const csvDelim = ' '
+const csvComment = '#'
+const datefmt = "02.01.2006" // dd.mm.yyyy
+const timefmt = "1504"       // hhmm
+const timefmtfull = "150405" // hhmmss
+
 type Entry struct {
-	date  string
-	start string
-	end   string
-	job   string
+	date     string
+	start    string
+	end      string
+	duration string
+	job      string
 }
 
 func main() {
 	fn := os.Args[1]
-	file, err := os.Open(fn)
+	csvContent := csvReader(fn)
+	data := csvData(csvContent)
+
+	for i := range data {
+		fmt.Println(data[i].duration)
+	}
+}
+
+func diff(start string, end string, format string) string {
+	s, err := time.Parse(format, start)
+	if err != nil {
+		fmt.Println(err)
+	}
+	e, err := time.Parse(format, end)
+	if err != nil {
+		fmt.Println(err)
+	}
+	diff := e.Sub(s)
+	return diff.String()
+}
+
+func csvData(input [][]string) []Entry {
+	var tmpEntry Entry
+	var allEntries []Entry
+	for _, entry := range input {
+		tmpEntry.date = entry[0]
+		tmpEntry.start = entry[1]
+		tmpEntry.end = entry[2]
+		tmpEntry.duration = diff(entry[1], entry[2], timefmt)
+		tmpEntry.job = entry[3]
+		allEntries = append(allEntries, tmpEntry)
+	}
+	return allEntries
+}
+
+func csvReader(fileName string) [][]string {
+	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
 
 	r := csv.NewReader(file)
-	r.Comma = ' '
-	r.Comment = '#'
+	r.Comma = csvDelim
+	r.Comment = csvComment
 
 	content, err := r.ReadAll()
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	var tmpEntry Entry
-	var allEntries []Entry
-	for _, entry := range content {
-		tmpEntry.date = entry[0]
-		tmpEntry.start = entry[1]
-		tmpEntry.end = entry[2]
-		tmpEntry.job = entry[3]
-		allEntries = append(allEntries, tmpEntry)
-	}
-	fmt.Println(allEntries[1].start)
+	return content
 }
