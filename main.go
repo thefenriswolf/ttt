e5a57ab6c7f5658545ab6a1e07bb398bfa2c9776
thefenriswolf <stefan.rohrbacher97@gmail.com>
2023-12-26 18:23:24 +0100

    update icon, update readme

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import ()
 
 func main() {
 	argParse() // hand of operation to the cli part of the program
+	// #todo: add text to logo, see: https://github.com/zyedidia/micro/blob/master/assets/micro-logo-drop.svg
 	// #todo: html export
 	// #todo: graphs
 	// #todo: organization
